f6a17c420b46f5dbde6edb4187044dad05fcc487
Alan <alan.khanaev@gmail.com>
2017-07-28 19:35:47 +0100

    update application, added ejs template

--- app.js
+++ app.js
@@ -3,16 +3,27 @@ var express = require('express');
 var app = express();
 
 var port = 5000;
+var bookRouter = express.Router();
+
+app.set("views", "./src/views/");
 
 app.use(express.static('public'));
-app.use(express.static('src/views'));
+app.set('view engine', 'ejs');
+
+bookRouter.route("/")
+    .get(function (req, res) {
+        res.render('books', { nav: [{ Link: '/Books', Text: 'Books' }, { Link: '/Authors', Text: 'Authors' }] });
+    });
+
+bookRouter.route("/single")
+    .get(function (req, res) {
+        res.send("Hello Single Books");
+    });
+
+app.use("/Books", bookRouter);
 
 app.get('/', function (req, res) {
-    res.send('Hello World');
-});
-
-app.get('/books', function (req, res) {
-
+    res.render('index', { nav: [{ Link: '/Books', Text: 'Books' }, { Link: '/Authors', Text: 'Authors' }] })
 });
 
 app.listen(port, function (err) {
